d62651556206a9e0ed16b1df4c51f34a855068d5
Holy Crusader <holy.tanukis@gmail.com>
2023-07-03 18:34:15 +0200

    New random User-Agent

--- lib/src/util/http_client.dart
+++ lib/src/util/http_client.dart
@@ -1,4 +1,5 @@
 import 'package:http/http.dart' as http;
+import 'package:faker/faker.dart';
 
 class HttpClient extends http.BaseClient {
   final http.Client _client;
@@ -7,8 +8,21 @@ class HttpClient extends http.BaseClient {
 
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) async {
-    request.headers['User-Agent'] =
-        'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 289.0.0.25.49 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)';
+    final faker = Faker();
+
+    final versions = [
+      '289.0.0.25.49',
+      '278.0.0.22.117',
+      '277.0.0.17.107',
+      '269.0.0.18.75',
+      '268.0.0.14.75',
+      '265.0.0.19.301'
+    ];
+
+    var userAgent =
+        '${faker.internet.userAgent()} Mobile/15E148 Instagram ${versions[faker.randomGenerator.integer(versions.length - 1)]}';
+
+    request.headers['User-Agent'] = userAgent;
     request.headers['Origin'] = 'https://www.instagram.com';
     request.headers['Referer'] = 'https://www.instagram.com/';
 
